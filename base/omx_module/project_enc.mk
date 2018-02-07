a13558e2cdad4933773636a88d1f1cccfbb03469
Kevin Grandemange <kevin.grandemange@allegrodvt.com>
2018-02-07 11:01:04 +0100

    Fix commit 12b88508281a65861d84af14e16395fda50c716d: Compilation issue
    The roi support wasn't added as some roi file weren't actually compiled
    due to a bad merge.
    
    Signed-off-by: Kevin Grandemange <kevin.grandemange@allegrodvt.com>
    Signed-off-by: Dhaval Shah <dshah@xilinx.com>

--- base/omx_module/project_enc.mk
+++ base/omx_module/project_enc.mk
@@ -4,5 +4,6 @@ OMX_MODULE_ENC_SRCS+=\
 	$(THIS.omx_module_enc)/omx_module_enc.cpp\
 	$(THIS.omx_module_enc)/omx_device_enc_hardware_mcu.cpp\
 	$(THIS.omx_module_enc)/ROIMngr.cpp\
+	$(THIS.omx_module_enc)/omx_convert_module_soft_roi.cpp\
 
 UNITTESTS+=$(OMX_MODULE_ENC_SRCS)
