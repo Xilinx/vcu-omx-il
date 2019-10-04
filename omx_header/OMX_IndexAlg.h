b18ce87bfbf4962504df55a64c32a7f626686a0a
Arthur Vinchon <arthur.vinchon@allegrodvt.com>
2019-10-04 06:21:54 -0700

    introduce new index: OMX_ALG_IndexParamVideoMaxResolutionChange
    Signed-off-by: Arthur Vinchon <arthur.vinchon@allegrodvt.com>
    
    Acked-for-series:Varunkumar Allagadapa <varunkumar.allagadapa@xilinx.com>

--- omx_header/OMX_IndexAlg.h
+++ omx_header/OMX_IndexAlg.h
@@ -94,6 +94,7 @@ typedef enum OMX_ALG_INDEXTYPE
   OMX_ALG_IndexParamVideoMaxPictureSizes,             /**< reference: OMX_ALG_VIDEO_PARAM_MAX_PICTURE_SIZES */
   OMX_ALG_IndexParamVideoLoopFilterBeta,              /**< reference: OMX_ALG_VIDEO_PARAM_LOOP_FILTER_BETA */
   OMX_ALG_IndexParamVideoLoopFilterTc,                /**< reference: OMX_ALG_VIDEO_PARAM_LOOP_FILTER_TC */
+  OMX_ALG_IndexParamVideoMaxResolutionChange,         /**< reference: OMX_ALG_VIDEO_PARAM_MAX_RESOLUTION_CHANGE */
 
   /* Vendor Video configrations */
   OMX_ALG_IndexConfigVendorVideoStartUnused = OMX_IndexVendorStartUnused + 0x00380000,
