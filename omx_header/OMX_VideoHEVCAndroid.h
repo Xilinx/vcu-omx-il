17878e1474b4b5d85312cb6b912050b473286ef3
Arthur Vinchon <arthur.vinchon@allegrodvt.com>
2020-01-22 09:53:39 +0100

    OMX: Update to revision b12271
    Update copyrights
    
    Signed-off-by: Arthur Vinchon <arthur.vinchon@allegrodvt.com>

--- omx_header/OMX_VideoHEVCAndroid.h
+++ omx_header/OMX_VideoHEVCAndroid.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Allegro DVT2.  All rights reserved.
+ * Copyright (C) 2016-2020 Allegro DVT2.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
